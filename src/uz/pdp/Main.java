0094595132669254ba9ee468d8c9ec5337c7472f
getStuden <hayperpc.7@gmail.com>
2024-01-14 12:21:15 +0500

    Manu

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -1,10 +1,25 @@
 package uz.pdp;
 
+import uz.pdp.db.titel.Input;
+import uz.pdp.etitel.StudentServes;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("salom");
-        System.out.println("hayer");
+        while (true){
+            System.out.println( """
+                    1. Student qoshi
+                    2. Student korish
+                    3. Student ozgartirish
+                    4. Student ochirish
+                    """);
+            switch (Input.inputInt( "Tanlang : " )){
+                 case 1 -> StudentServes.Qoshi();
+                 case 2 -> StudentServes.Korish();
+                 case 3 -> StudentServes.Ozgartirish();
+                 case 4 -> StudentServes.Ochirish();
+            }
+        }
 
     }
 }
